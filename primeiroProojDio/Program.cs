4640ee50b9131ab3c66c57ac2d788d6eb4a6f5a3
marcoarrudajf <marcoarruda.dev@gmail.com>
2022-01-07 15:00:30 -0300

    Adicionar arquivos de projeto.

---
+++ primeiroProojDio/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace primeiroProojDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "world";
+            Console.WriteLine(a + " " + b);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
